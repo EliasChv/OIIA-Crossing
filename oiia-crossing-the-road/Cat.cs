031b6808e1d10f02d331d22f2b18f82750f7f95a
Elias <echavela@nd.edu>
2025-09-14 22:48:14 -0400

    Phase2.3
    Adding cat selection, orange / gray
    White cat needs texture update

--- oiia-crossing-the-road/Cat.cs
+++ oiia-crossing-the-road/Cat.cs
@@ -4,9 +4,14 @@ using System;
 public partial class Cat : Node3D
 {
     private Node3D CatNode;
+    [ Export ] private Texture2D[] CatTexture = [];
+    private int CurrentSkin = 0;
     public override void _Ready()
     {
-        CatNode = GetNode<Node3D>("cats");
+        CatNode = GetNode<Node3D>("Cat");
+
+        GetNode<Button>( "Control/HBoxContainer/right" ).Pressed += () => { ChangeTexture( 1 ); };
+        GetNode<Button>( "Control/HBoxContainer/left" ).Pressed += () => { ChangeTexture( -1 ); };
     }
 
     public override void _Process( double delta )
@@ -14,4 +19,8 @@ public partial class Cat : Node3D
         CatNode.RotateY( - ( float ) Math.PI * 2 * ( float ) delta );
     }
 
+    public void ChangeTexture( int number ) {
+        CurrentSkin = Math.Clamp( CurrentSkin + number, 0, CatTexture.Length - 1 );
+        CatNode.GetNode<MeshInstance3D>( "body" ).Mesh.SurfaceGetMaterial( 0 ).Set( "albedo_texture", CatTexture[ CurrentSkin ] );
+    }
 }
